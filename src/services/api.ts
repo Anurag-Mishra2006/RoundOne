53a8dfd8aeeea8b5b96db8b2863a804f2fe3a54f
anurag-mishra2006 <eranurag2006@gmail.com>
2026-06-21 01:08:15 +0530

    created an axios instance and all api call at one place

---
+++ src/services/api.ts
@@ -0,0 +1,38 @@
+//  Axios instance + all API call 
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:3000",
+    withCredentials : true
+});
+// auth calls
+export const signup = async (data: {name: string, email : string, password: string})=>{
+    return  await api.post("/user/signup", data);
+}
+export const login = async(data: {email : string, password: string})=>{
+    return await api.post("/user/login", data);
+}
+export const verifyOtp = async(data: {email: string, otp: string})=>{
+    return await api.post("/user/verify-otp", data);
+};
+export const resendOtp = async(data: {email: string})=>{
+    return await api.post("/user/resend-otp", data);
+};
+export const logout = async()=>{
+    return await api.post("/user/logout");
+};
+
+// resume time
+export const uploadResume = async(file : File)=>{
+    const formData = new FormData();
+    formData.append("resume", file);
+    return await api.post("/resume/upload", formData);
+}
+
+// interview 
+export const interviewStart = async(data: { company: string, role: string, level: string, language:string })=>{
+    return await api.post("/interview/start", data);
+}
+export const evaluateInterview = async(data: {round: string, company: string, question: string, answer: string})=>{
+    return await api.post("/interview/evaluate", data);
+}
