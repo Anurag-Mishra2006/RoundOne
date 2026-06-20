e4b8157849fb3feeba40ec917300539269883764
anurag-mishra2006 <eranurag2006@gmail.com>
2026-06-21 01:42:14 +0530

    exported all the basic needed interface

---
+++ src/types/index.ts
@@ -0,0 +1,61 @@
+export interface User{
+    id : string,
+    name : string, 
+    email : string,
+}
+
+export interface Question{
+    id : number,
+    question: string,
+    topic : string
+};
+
+export interface HRTechnicalRound{
+    round : string,
+    company: string,
+    role : string,
+    candidateName : string,
+    questions : Question[]
+};
+export interface Example{
+    input: string,
+    output: string,
+    explanation: string,
+}
+export interface Problem{
+    id: number,
+    title: string,
+    description: string,
+    examples: Example[],
+    constraints: string[],
+    topic: string,
+    difficulty: string
+}
+
+export interface DsaRound{
+    round: string,
+    company: string,
+    level: string,
+    language: string,
+    problem : Problem
+}
+
+export interface EvaluateResult{
+    round : string,
+    score: number,
+    maxScore: number,
+    feedback: string,
+    strongPoints: string[],
+    improvements:string[],
+    suggestion: string
+}
+
+export interface InterviewStartResponse {
+  hr: HRTechnicalRound
+  technical: HRTechnicalRound
+  dsa: DsaRound
+}
+
+export interface EvaluateResponse {
+  evaluateResult: EvaluateResult
+}
