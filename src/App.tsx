950c4ffdf17f00e936fa887d1f78ea536c453719
anurag-mishra2006 <eranurag2006@gmail.com>
2026-06-27 22:56:48 +0530

    resolve issue of everytime login for hydrating zustand session

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,33 @@ import Onboarding from './pages/Onboarding'
 import Interview from './pages/Interview'
 import Feedback from './pages/Feedback'
 import Navbar from "./components/Navbar"
-import AtsChecker from './pages/AtsChecker'
+import AtsChecker from './pages/AtsChecker';
+import { getMe } from './services/api'
+import useUserStore from './store/authStore'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const { isAuthenticate, setUser, clearUser } = useUserStore();
+  const [isAuthCheck, setIsAuthCheck] = useState(true);
 
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        const response = await getMe();
+        if (response.status === 200) {
+          setUser(response.data.user);
+        }
+      } catch (error) {
+        clearUser();
+      } finally {
+        setIsAuthCheck(false);
+      }
+    }
+    checkAuthStatus()
+  }, [])
+  if (isAuthCheck) {
+    return <div className="min-h-screen bg-[var(--bg)] flex items-center justify-center text-[var(--text)]">Loading...</div>;
+  }
   return (
     <>
       <BrowserRouter>
@@ -19,13 +42,15 @@ function App() {
         <Routes>
           <Route path='/register' element={<Registration />} />
           <Route path='/verify-otp' element={<VerifyOtp />} />
-          <Route path='/login' element={<Login />} />
+          {/* <Route path='/login' element={<Login />} /> */}
+          <Route path='/login' element={isAuthenticate ? <Navigate to="/resume-upload" /> : <Login />} />
+
           <Route path='/resume-upload' element={<ResumeUpload />} />
           <Route path='/onboarding' element={<Onboarding />} />
           <Route path='/interview' element={<Interview />} />
           <Route path='/feedback' element={<Feedback />} />
           <Route path='/ats-check' element={<AtsChecker />} />
-          <Route path='/' element={<Navigate to="/login" />} />
+          <Route path='/' element={isAuthenticate? <Navigate to="/resume-upload" />: <Login/>} />
         </Routes>
       </BrowserRouter>
     </>
