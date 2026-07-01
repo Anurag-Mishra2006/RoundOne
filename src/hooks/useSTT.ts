160c463e8d9c902044d13624fb40f22502c07156
anurag-mishra2006 <eranurag2006@gmail.com>
2026-07-01 21:15:17 +0530

    add: STT feature

---
+++ src/hooks/useSTT.ts
@@ -0,0 +1,77 @@
+import { useRef, useState } from "react"
+import { transcribeAudio } from "@/services/api.js"
+
+export const useSTT = () => {
+    const [isRecording, setIsRecording] = useState<boolean>(false);
+    const [isProcessing, setIsProcessing] = useState<boolean>(false);
+
+    const mediaRecoderRef = useRef<MediaRecorder | null>(null);
+    const chunkRef = useRef<Blob[]>([])
+
+    const startRecording = async () => {
+        try {
+            // ask for mic permission 
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+            // create MediaRecorder -> this contains the blobs of audio
+            const mediaRecoder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
+            mediaRecoderRef.current = mediaRecoder;
+            chunkRef.current = [];
+
+            // when audio data avaialbe push it to chunk
+            mediaRecoder.ondataavailable = ((e) => {
+                if (e.data.size > 0) {
+                    chunkRef.current.push(e.data);
+                }
+            });
+
+            // start recording
+            mediaRecoder.start();
+            setIsRecording(true);
+
+        } catch (error) {
+            console.error("Microphone access denied or error", error);
+            alert("Please allow microphone access to use this feature.");
+
+        }
+    };
+    const stopRecording = (): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            if (!mediaRecoderRef.current) {
+                reject("No media recorder initialized");
+                return;
+            }
+
+            //   MUST WRAP THE LOGIC IN .onstop!
+            mediaRecoderRef.current.onstop = async () => {
+                setIsRecording(false);
+                setIsProcessing(true);
+
+                const audioBlob = new Blob(chunkRef.current, { type: 'audio/webm' });
+                mediaRecoderRef.current?.stream.getTracks().forEach(track => track.stop());
+
+                try {
+                    const response = await transcribeAudio(audioBlob);
+
+                    if (response.status !== 200) {
+                        reject("Transcription Failed")
+                    }
+                    // Axios puts the JSON response inside `response.data`.
+                    // Your backend returns { transcript: "..." }, so we need response.data.transcript
+                    resolve(response.data.transcript);
+
+                } catch (error) {
+                    console.error(error)
+                    reject(error)
+                } finally {
+                    setIsProcessing(false); // 3. Fixed typo: was isProcessing(false)
+                }
+            };
+
+            // Trigger the stop, which triggers the .onstop event above!
+            mediaRecoderRef.current.stop();
+        });
+    }
+    return {isProcessing, isRecording, startRecording, stopRecording}
+
+}
